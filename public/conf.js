227c76b3c6bef05cbe2874df85f3879cb7d265b2
LCHNAN <40926270+chenglun11@users.noreply.github.com>
2025-06-24 18:03:23 +0100

    Update conf.js

--- public/conf.js
+++ public/conf.js
@@ -2,7 +2,8 @@
 
 //接口地址
 window.server_ip = {
-     BASE_URL:"http://v6.ca7.asia:9001/api",
+     // 自行修改
+     // BASE_URL:"http://v6.ca7.asia:9001/api",
     // BASE_URL:"http://47.103.39.109:15465/api",
     // BASE_URL:"http://192.168.11.235:15465/api",
     // BASE_URL:"http://192.168.11.105:15465/api",
